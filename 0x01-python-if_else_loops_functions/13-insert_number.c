36a5e464447eb7ba54df3e65f08a6affc082e97c
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-08 16:52:18 -0700

    added files

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,42 +7,38 @@
  *
  * Return: A pointer to the updated list, or NULL if memory allocation fails.
  **/
+
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *current, *new_node;
+	listint_t *old_node, *new_node;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	current = *head;
-
+	old_node = *head;
 	if (*head == NULL)
 	{
 		new_node = add_nodeint_end(head, number);
 		return (new_node);
 	}
-
-	if (number < current->n)
+	if (number < old_node->n)
 	{
-		new_node->next = current;
+		new_node->next = old_node;
 		new_node->n = number;
 		*head = new_node;
 		return (new_node);
 	}
-
-	while (current->next)
+	while (old_node->next)
 	{
-		if (number < current->next->n)
+		if (number < old_node->next->n)
 		{
-			new_node->next = current->next;
-			current->next = new_node;
+			new_node->next = old_node->next;
+			old_node->next = new_node;
 			new_node->n = number;
 			return (new_node);
 		}
-		current = current->next;
+		old_node = old_node->next;
 	}
-
 	new_node = add_nodeint_end(head, number);
 	return (new_node);
 }
