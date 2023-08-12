bc46c5a88c2f20b8393bf77b6552a0a7c73d1791
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-11 18:47:42 -0700

    added files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
-    # Import the add function from the add_0 module
+    """Print the summation of 1 and 2."""
     from add_0 import add
 
-    # Define operands
     a = 1
     b = 2
-
-    # Calculate the answer using the add function
-    answer = add(a, b)
-
-    # Print the addition equation and the answer
-    print("{} + {} = {}".format(a, b, answer))
+    print("{} + {} = {}".format(a, b, add(a, b)))
