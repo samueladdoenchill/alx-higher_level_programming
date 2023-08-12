ab8ded650fab8291feb8ad6dcd7628dd45a58c48
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-11 18:52:18 -0700

    aded files

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 
-# Entry point of the script
+"""
+A program that imports functions from a calculator module,
+performs mathematical operations, and displays the results.
+"""
+
 if __name__ == "__main__":
-    # Import functions from calculator_1 module
+    # Import functions from the calculator_1 module
     from calculator_1 import add, sub, mul, div
 
     # Define operands
     a = 10
     b = 5
 
-    # Perform addition and print the result
-    print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+    # Perform addition and display the result
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-    # Perform subtraction and print the result
-    print("{:n} - {:n} = {:n}".format(a, b, sub(a, b)))
+    # Perform subtraction and display the result
+    print("{} - {} = {}".format(a, b, sub(a, b)))
 
-    # Perform multiplication and print the result
-    print("{:n} * {:n} = {:n}".format(a, b, mul(a, b)))
+    # Perform multiplication and display the result
+    print("{} * {} = {}".format(a, b, mul(a, b)))
 
-    # Perform division and print the result
-    print("{:n} / {:n} = {:n}".format(a, b, div(a, b)))
+    # Perform division and display the result
+    print("{} / {} = {}".format(a, b, div(a, b)))
