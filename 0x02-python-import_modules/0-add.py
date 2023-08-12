fbff1ab7e47636987cbf9c52d29fb3c5370b76d3
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-11 18:33:39 -0700

    added files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Entry point of the script."""
-
-    # Import the add function from the add_0 module.
+    # Import the add function from the add_0 module
     from add_0 import add
 
-    # Define variables for the operands.
+    # Define operands
     a = 1
     b = 2
 
-    # Calculate the sum using the add function and print the result.
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    # Calculate the answer using the add function
+    answer = add(a, b)
+
+    # Print the addition equation and the answer
+    print("{} + {} = {}".format(a, b, answer))
