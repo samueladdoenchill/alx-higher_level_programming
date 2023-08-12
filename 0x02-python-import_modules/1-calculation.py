46edacd92e162bee54a95c289e04a859876ad865
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-11 18:54:49 -0700

    added files

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,21 +6,10 @@ performs mathematical operations, and displays the results.
 """
 
 if __name__ == "__main__":
-    # Import functions from the calculator_1 module
     from calculator_1 import add, sub, mul, div
-
-    # Define operands
     a = 10
     b = 5
-
-    # Perform addition and display the result
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-    # Perform subtraction and display the result
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    # Perform multiplication and display the result
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-    # Perform division and display the result
     print("{} / {} = {}".format(a, b, div(a, b)))
