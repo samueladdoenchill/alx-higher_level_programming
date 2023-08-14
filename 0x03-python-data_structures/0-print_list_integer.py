dd0931312602dabd50c4b0ba0ac2967e694e06a3
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-14 15:30:26 -0700

    added 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# Define a function that prints integers from a list
-def print_list_integers(my_list=[]):
-    # Iterate through the elements in the list
+def print_list_integer(my_list=[]):
     for int_value in my_list:
-        # Print each integer element
         print("{:d}".format(int_value))
