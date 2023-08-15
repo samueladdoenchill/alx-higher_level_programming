acf317a131da7070d35ca35c8ad65ac54e369122
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-14 17:18:34 -0700

    added 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    """
+    Determine divisibility by 2 for each element in the list.
+    
+    Args:
+        my_list (list): The input list of integers.
+        
+    Returns:
+        A new list of booleans indicating whether each element is divisible by 2.
+    """
+    if not my_list:
+        return
+    result_list = []
+    for value in my_list:
+        if value % 2 == 0:
+            result_list.append(True)
+        else:
+            result_list.append(False)
+    return result_list
