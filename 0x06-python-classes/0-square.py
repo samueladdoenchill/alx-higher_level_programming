7a5805e5f2cfabd383dd9a9458f27c85e68f7c2c
samueladdoenchill <samueladdoenchill@gmail.com>
2023-08-29 18:17:45 +0000

    added more files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-This script defines a class called Square.
-"""
+"""This script defines a class called Square."""
 
 
 class Square:
-    """
-    This class represents a geometric square.
-    """
+    """This class represents a geometric square."""
     pass
